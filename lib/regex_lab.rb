c042e8c21cd6203fda6229ae5ee9504ed4c88065
Jenny K <jkam17@gmail.com>
2018-08-21 02:46:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9]{10}/) ? TRUE : FALSE
+  phone.match(/\d{3}[ .)-]?\d{3}[ .-]?\d{4}\z/) ? TRUE : FALSE
 end
