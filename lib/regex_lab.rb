cf82c944c23cff7e76c6936962574336b101f74e
Jenny K <jkam17@gmail.com>
2018-08-21 02:46:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A+[upper:]+.+[.?!]+\z/) ? TRUE : FALSE
+  text.match(/\A+[A-Z]+.+[.?!]+\z/) ? TRUE : FALSE
 end
 
 def valid_phone_number?(phone)
